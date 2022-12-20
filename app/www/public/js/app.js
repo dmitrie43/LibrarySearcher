c7bc3370b85df4916924c5437bca49cf9753afd7
Dmitry <dmitrie43@mail.ru>
2022-12-20 23:01:07 +0300

    added layouts

---
+++ app/www/public/js/app.js
@@ -0,0 +1,20 @@
+//require('./bootstrap');
+
+$(function() {
+    $(window).scroll(function(){
+        var scrolltop=$(this).scrollTop();
+        if(scrolltop>=200){
+            $("#elevator_item").show();
+        }else{
+            $("#elevator_item").hide();
+        }
+    });
+    $("#elevator").click(function(){
+        $("html,body").animate({scrollTop: 0}, 500);
+    });
+    $(".qr").hover(function(){
+        $(".qr-popup").show();
+    },function(){
+        $(".qr-popup").hide();
+    });
+});
