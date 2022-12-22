00f1b1ec4f722dda5062c8b44c66ec8b4f1fc600
Dmitry <dmitrie43@mail.ru>
2022-12-22 23:08:55 +0300

    added controllers

--- app/www/app/Http/Controllers/IndexController.php
+++ app/www/app/Http/Controllers/IndexController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Repository\IBookRepository;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    private IBookRepository $bookRepository;
+
+    public function __construct(IBookRepository $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
+    }
+
     public function index()
     {
-        return view('index');
+        $books = $this->bookRepository->all();
+        return view('index', compact('books'));
     }
 }
