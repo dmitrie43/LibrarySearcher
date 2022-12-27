5b67e9cfb21931b826959400fab6a68c4ea18b17
Dmitry <dmitrie43@mail.ru>
2022-12-27 16:48:24 +0400

    added ru validation and login form

--- app/www/resources/views/auth/login.blade.php
+++ app/www/resources/views/auth/login.blade.php
@@ -1,3 +1,36 @@
+@include ("header")
+
+<div class="bannerSec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bannercntSec signcntsec">
+                    <div class="signctn">
+                        <h2 class="textwhitecolor signheading">Авторизация <span class="textbluecolor">libSearcher</span></h2>
+                        <div class="sign-from">
+                            <x-auth-validation-errors class="mb-4 alert-danger" :errors="$errors" />
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="input-box">
+                                    <input id="email" type="email" name="email" placeholder="Email" required class="form-input">
+                                </div>
+                                <div class="input-box">
+                                    <input id="password" type="password" name="password" placeholder="Пароль" required class="form-input">
+                                </div>
+                                <div class="sign-btn">
+                                    <button class="btn btnlightblue me-3" type="submit">Вход</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include ("footer")
+<? /*
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -54,3 +87,4 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+*/?>
