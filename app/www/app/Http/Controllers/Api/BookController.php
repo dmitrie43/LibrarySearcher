cf847a7a4df8e16ca90488742672ba4ffa9ced1c
Dmitry <dmitrie43@mail.ru>
2023-01-05 19:01:01 +0400

    api books

---
+++ app/www/app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Repository\IBookRepository;
+use App\Repository\IGenreRepository;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    private IBookRepository $bookRepository;
+    private IGenreRepository $genreRepository;
+
+    public function __construct(
+        IBookRepository $bookRepository,
+        IGenreRepository $genreRepository
+    )
+    {
+        $this->bookRepository = $bookRepository;
+        $this->genreRepository = $genreRepository;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function get(Request $request)
+    {
+        try {
+            $request->validate([
+                "genre" => ['nullable', 'integer'],
+                "limit" => ['nullable', 'integer'],
+            ]);
+
+            $limit = isset($request->limit) ? $request->limit : 10;
+
+            if ($request->has('genre')) {
+                $genre = $this->genreRepository->find($request->get('genre'));
+                $books = $this->bookRepository->getByGenre($genre, $limit);
+            } else {
+                $books = $this->bookRepository->all()->take($limit);
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'books' => $books,
+                ],
+            ], 200);
+        } catch (\Throwable $throwable) {
+            return response()->json([
+                'success' => false,
+                'message' => $throwable->getMessage(),
+            ], 500);
+        }
+    }
+}
--- app/www/routes/api.php
+++ app/www/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/auth/login', [AuthController::class, 'login']);
-
 Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 });
 
+Route::group(['prefix' => '/book'], function() {
+    Route::get('/get', [BookController::class, 'get']);
+});
