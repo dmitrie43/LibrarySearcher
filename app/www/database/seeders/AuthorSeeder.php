af9e4cce3e8bbb84dbc3cac9afd4318b87740adb
Dmitry <dmitrie43@mail.ru>
2025-01-17 20:41:52 +0400

    author photo

--- app/www/database/seeders/AuthorSeeder.php
+++ app/www/database/seeders/AuthorSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Helpers\FileUploader;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
 
 class AuthorSeeder extends Seeder
@@ -15,15 +17,41 @@ class AuthorSeeder extends Seeder
     public static function run()
     {
         $authors = [
-            'Анджей Сапковский',
-            'Рэй Брэдбери',
-            'Стивен Кинг',
-            'Ричард Мэтисон',
-            'Джордж Оруэлл',
+            'Анджей Сапковский' => [
+                'photo' => FileUploader::upload(
+                    new UploadedFile(path: public_path('/img/Sapkovskie.png'), originalName: 'Sapkovskie'),
+                    FileUploader::IMAGE_PATH
+                ),
+            ],
+            'Рэй Брэдбери' => [
+                'photo' => FileUploader::upload(
+                    new UploadedFile(path: public_path('/img/bredbery.jpg'), originalName: 'Bredbery'),
+                    FileUploader::IMAGE_PATH
+                ),
+            ],
+            'Стивен Кинг' => [
+                'photo' => FileUploader::upload(
+                    new UploadedFile(path: public_path('/img/stiven-king.webp'), originalName: 'StivenKing'),
+                    FileUploader::IMAGE_PATH
+                ),
+            ],
+            'Ричард Мэтисон' => [
+                'photo' => FileUploader::upload(
+                    new UploadedFile(path: public_path('/img/richardM.jpg'), originalName: 'RichardM'),
+                    FileUploader::IMAGE_PATH
+                ),
+            ],
+            'Джордж Оруэлл'=> [
+                'photo' => FileUploader::upload(
+                    new UploadedFile(path: public_path('/img/Oruel.jpg'), originalName: 'JorgeO'),
+                    FileUploader::IMAGE_PATH
+                ),
+            ],
         ];
-        foreach ($authors as $author) {
+        foreach ($authors as $authorName => $authorData) {
             DB::table('authors')->insert([
-                'full_name' => $author,
+                'full_name' => $authorName,
+                'photo' => $authorData['photo'] ?? null,
             ]);
         }
     }
